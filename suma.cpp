3ddcd8169b84f00b4e8e1849d27d4ddd04349444
jdburgosr <jdburgosr@unal.edu.co>
2018-02-17 00:32:27 -0500

    borland

---
+++ suma.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio>
+void main (void)
+{
+double a(0),b(0);
+printf("ingrese los numeros a sumar, primer numero:\n");
+scanf("%lf",&a);
+printf("ingrese el segundo numero:\n");
+scanf("%lf", &b);
+printf("la suma de %lf+%lf=%lf\n",a,b,a+b);
+getch();
+}
+
