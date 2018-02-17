0e0ece1980ec5d9362447e10ddee3921c4b91575
jdburgosr <30726369+jdburgosr@users.noreply.github.com>
2018-02-17 00:46:53 -0500

    borland

---
+++ ejemplo1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio>
+void main()
+{
+double a(9),b,c;
+printf("el valor de a=");
+scanf("%lf",&a);
+printf("el valor de b=");
+scanf("%lf",&b);
+printf("el valor de c=");
+scanf("%lf",&c);
+printf("\n %lf x*x + %lf x + %lf = 0 \n\n", a, b, c);
+getch();
+}
---
+++ tarea1.cpp
@@ -0,0 +1,12 @@
+#include <conio>
+#include <iostream>
+void main(void)
+{
+ double a(0),b(0),c(0);
+ a=12.03;
+ b=-13.05;
+ c=a+b;
+ cout<<"la suma de "<<a<<" mas "<<b<<" da como resultado "<<c<<endl;
+ //printf("la suma de %i\n",a); no imprime lo esperado
+ getch();
+ }
